e801cced5e93513928a1bbbdad779e2ff91e67d9
MadennFloss <47519011+MadennFloss@users.noreply.github.com>
2019-05-27 00:50:31 +0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+include<iostream> include<string> using namespace std;
+int
+main()
+{
+  string name; //Объявление переменной имени пользователя.
+  cout << "What is your name?";
+  cin >> name; //Ввод имени пользователя.
+  cout << "Hello World from " << name; //Вывод приветствия с именем пользователя.
+  return 0;
+}
+#homework2
